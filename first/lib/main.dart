e17686bdb0f07379f60a859350c6eaa5166a49dc
Pratama Yoga <pratamayo16@gmail.com>
2019-11-23 20:02:31 +0700

    Instagram duplicate

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,103 +10,124 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// Kerangka
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Instagram'),
+          centerTitle: true,
+          backgroundColor: Colors.indigo,
+          actions: <Widget>[Icon(Icons.add_a_photo)],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 2,
+                            color: Colors.redAccent,
+                            style: BorderStyle.solid
+                          ),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+
+                      child: ClipOval(
+                        child: CachedNetworkImage(
+                          imageUrl: 'https://iv1.lisimg.com/image/4898355/321full-toru-yamashita.jpg',
+                        )
+                  )),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  // Untuk responsive
+                  Expanded(child: Text('pratamays')),
+                  Icon(Icons.more_vert)
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                child:  Image.network(
+                  'https://a.wattpad.com/cover/79427568-288-k268148.jpg'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.favorite_border),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.comment),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                      child: Container(
+                          alignment: Alignment.centerLeft,
+                          child: Icon(Icons.send))),
+                  Icon(Icons.bookmark),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 10, left: 10),
+                  color: Colors.yellow,
+                  child: Text('Liked by shahnazasyf and 100 others')),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                child: RichText(
+                  text: TextSpan(children: [
+                    TextSpan(
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                        text: 'pratamays '),
+                    TextSpan(
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                        text: ' Waaaaa toru keren bet dah'),
+                    TextSpan(
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                        text: ' #OneOkeRock')
+                  ]),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
+
+// Body
